8fe73be9159061f33e031d47180578885a838a09
Andrew Glines and Joshua Fairchild <andrew+joshua@>
2017-06-01 09:17:39 -0700

    wrote and failed first test ; wrote code to pass test ; test ensures that one-point letters are accurately scored

--- Objects/ScoreGenerator.cs
+++ Objects/ScoreGenerator.cs
@@ -8,7 +8,36 @@ namespace ScrabbleScore
   {
     public static int GenerateScore(string userInput)
     {
-      return 0;
+      int score = 0;
+      // alphabet as list of chars
+      // string alphabet = "abcdefghijklmnopqrstuvwxyz";
+      // char[] alphabetAsCharArray = alphabet.ToCharArray();
+      // user input as string
+        // is already the argument of the method
+      // convert to char array
+      string inputToLower = userInput.ToLower();
+      char[] inputAsCharArray = inputToLower.ToCharArray();
+
+      // int for scoring
+      int Oneletterscore = 1;
+      int Twoletterscore = 2;
+      int Threeletterscore = 3;
+      int Fourletterscore = 4;
+      int Fiveletterscore = 5;
+      int Eightletterscore = 8;
+      int Tenletterscore = 10;
+
+      // for loop over input array, sum score @ each hit
+      for (int i = 0; i < inputAsCharArray.Length ; i++)
+      {
+        if (inputAsCharArray[i] == 'a' || inputAsCharArray[i] == 'e' || inputAsCharArray[i] == 'i' || inputAsCharArray[i] == 'o' || inputAsCharArray[i] == 'u' || inputAsCharArray[i] == 'l' || inputAsCharArray[i] == 'n' || inputAsCharArray[i] == 'r' || inputAsCharArray[i] == 's' || inputAsCharArray[i] == 't')
+        {
+          score += Oneletterscore;
+        }
+      }
+      // produce score
+      return score;
+
     }
   }
 }
--- Tests/ScoreGeneratorTests.cs
+++ Tests/ScoreGeneratorTests.cs
@@ -6,7 +6,17 @@ namespace ScrabbleScore
 {
   public class ScoreGeneratorTests
   {
-    
+    [Fact]
+    public void GenerateScore_CountsLetterScore_1PointLetters()
+    {
+      //Arrange
+      string testInput = "abcdefghijklmnopqrstuvwxyz";
+      int  testScore = 10;
+      //Act
+      int result = ScoreGenerator.GenerateScore(testInput);
+      //Assert
+      Assert.Equal(testScore, result);
+    }
   }
 
 

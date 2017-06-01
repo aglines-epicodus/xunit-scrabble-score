ac42a29c2d9ba39d7a4ab3761bc6e6bf37b02a5a
Andrew Glines and Joshua Fairchild <andrew+joshua@>
2017-06-01 11:09:00 -0700

    add form and route for displaying output ; add css styling

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,7 +9,19 @@ namespace ScrabbleScore
     public HomeModule()
     {
       //code
-      Get["/"] = _ => "hello world!";
+      Get["/"] = _ => View["index.cshtml"];
+
+      Post["/"] = _ => {
+        string userInput = Request.Form["user-input"];
+        int score = ScoreGenerator.GenerateScore(userInput);
+        string scoreAsString = score.ToString();
+
+        Dictionary<string, string> model = new Dictionary<string, string>{};
+
+        model.Add("input", userInput);
+        model.Add("score", scoreAsString);
+        return View["index.cshtml", model];
+      };
     }
   }
 }
